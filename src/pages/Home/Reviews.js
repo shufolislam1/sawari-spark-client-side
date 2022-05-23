faa9785da91920e0ebe1f8bdacacb8df47ce7fe4
shufol islam <shufolislam1255@gmail.com>
2022-05-23 19:25:17 +0600

    reviews design done

--- src/pages/Home/Reviews.js
+++ src/pages/Home/Reviews.js
@@ -2,31 +2,25 @@ import React from 'react';
 
 const Reviews = () => {
     return (
-        <div className='grid sm:grid-cols-1 lg:grid-cols-3 gap-4 my-20'>
-            <div class="cardb bg-base-100 shadow-xl">
-                <div class="card-body">
-                    <h2 class="card-title">Card title!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div class="card-actions justify-end">
-                        <button class="btn btn-primary">Buy Now</button>
+        <div className='grid'>
+            <h2 className='text-4xl font-bold justify-self-center mt-4'>Reviews</h2>
+            <div className='grid sm:grid-cols-1 lg:grid-cols-3 gap-4 my-20'>
+                <div class="cardb bg-base-100 shadow-xl">
+                    <div class="card-body">
+                        <h2 class="card-title">Card title!</h2>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
                     </div>
                 </div>
-            </div>
-            <div class="card bg-base-100 shadow-xl">
-                <div class="card-body">
-                    <h2 class="card-title">Card title!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div class="card-actions justify-end">
-                        <button class="btn btn-primary">Buy Now</button>
+                <div class="card bg-base-100 shadow-xl">
+                    <div class="card-body">
+                        <h2 class="card-title">Card title!</h2>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
                     </div>
                 </div>
-            </div>
-            <div class="card bg-base-100 shadow-xl">
-                <div class="card-body">
-                    <h2 class="card-title">Card title!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div class="card-actions justify-end">
-                        <button class="btn btn-primary">Buy Now</button>
+                <div class="card bg-base-100 shadow-xl">
+                    <div class="card-body">
+                        <h2 class="card-title">Card title!</h2>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
                     </div>
                 </div>
             </div>
