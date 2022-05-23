bd1179ee48ba4571b3b4823aaa82d6e73f561dae
shufol islam <shufolislam1255@gmail.com>
2022-05-24 01:27:45 +0600

    review add part done

--- src/pages/BookOrder/AddReview.js
+++ src/pages/BookOrder/AddReview.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const AddReview = () => {
+    const { register, handleSubmit } = useForm();
+    const onSubmit = (data) =>{ 
+        console.log(data)
+
+        // send data to server
+        const url = `http://localhost:5000/spark`
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        .then(res => res.json())
+        .then (result => {
+            console.log(result);
+        })
+    };
     return (
         <div>
-            <h2>add review</h2>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input {...register("description")} />
+                <input type="number" {...register("ratings", { min: 1, max: 5 })} />
+                <input type="submit" value="Add Spark" />
+            </form>
         </div>
     );
 };
